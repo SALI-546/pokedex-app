7636be0d30b7e8bd0ee9c323c4a28bb3e58b966f
msalifou <msalifou@innovqube.com>
2025-03-15 11:43:41 +0100

    Add Tailwind CSS CDN to test styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'; 
+import Script from 'next/script'; // Importer le composant Script
+import '../styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Charger le CDN de Tailwind CSS avec le composant Script */}
+        <Script
+          src="https://cdn.tailwindcss.com"
+          strategy="afterInteractive" 
+        />
+       
+      </head>
       <body>{children}</body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', // Inclut tous les fichiers dans src/
+    './src/**/*.{js,ts,jsx,tsx}', 
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
